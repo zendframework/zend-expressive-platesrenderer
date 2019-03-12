1917e7a1ef674ad93c9a7d7678f12caee95332de
Matthew Weier O'Phinney <matthew@zend.com>
2019-03-12 13:37:32 -0500

    qa: remove unused import, update license year

--- test/Extension/UrlExtensionTest.php
+++ test/Extension/UrlExtensionTest.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @see       https://github.com/zendframework/zend-expressive-platesrenderer for the canonical source repository
- * @copyright Copyright (c) 2016-2017 Zend Technologies USA Inc. (https://www.zend.com)
+ * @copyright Copyright (c) 2016-2019 Zend Technologies USA Inc. (https://www.zend.com)
  * @license   https://github.com/zendframework/zend-expressive-platesrenderer/blob/master/LICENSE.md New BSD License
  */
 
@@ -12,7 +12,6 @@ namespace ZendTest\Expressive\Plates\Extension;
 use League\Plates\Engine;
 use PHPUnit\Framework\TestCase;
 use Prophecy\Prophecy\ProphecyInterface;
-use ReflectionMethod;
 use Zend\Expressive\Helper\ServerUrlHelper;
 use Zend\Expressive\Helper\UrlHelper;
 use Zend\Expressive\Plates\Extension\UrlExtension;
