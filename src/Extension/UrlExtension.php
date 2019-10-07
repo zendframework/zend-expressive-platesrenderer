03687c88377e56b84fff9764b4c6882ed3c0b8d9
pine3ree <pine3ree@gmail.com>
2019-10-07 22:08:08 +0200

    add back the removed redundant function (and related tests) to avoid BC break, since they had public visibility, but mark them as deprecated and scheduled to be removed in next major version.

--- src/Extension/UrlExtension.php
+++ src/Extension/UrlExtension.php
@@ -51,4 +51,50 @@ class UrlExtension implements ExtensionInterface
         $engine->registerFunction('serverurl', $this->serverUrlHelper);
         $engine->registerFunction('route', [$this->urlHelper, 'getRouteResult']);
     }
+
+    /**
+     * Get the RouteResult instance of UrlHelper, if any.
+     *
+     * @deprecated since 2.2.0; to be removed in 3.0.0. This method was originally
+     *     used internally to back the route() Plates function; we now register
+     *     the UrlHelper::getRouteResult callback directly.
+     */
+    public function getRouteResult() : ?RouteResult
+    {
+        return $this->urlHelper->getRouteResult();
+    }
+
+    /**
+     * Generate a URL from either the currently matched route or the specfied route.
+     *
+     * @param array  $options Can have the following keys:
+     *     - router (array): contains options to be passed to the router
+     *     - reuse_result_params (bool): indicates if the current RouteResult
+     *       parameters will be used, defaults to true
+     * @return string
+     * @deprecated since 2.2.0; to be removed in 3.0.0. This method was originally
+     *     used internally to back the url() Plates function; we now register
+     *     UrlHelper instance directly, as it is callable.
+     */
+    public function generateUrl(
+        string $routeName = null,
+        array $routeParams = [],
+        array $queryParams = [],
+        ?string $fragmentIdentifier = null,
+        array $options = []
+    ) {
+        return $this->urlHelper->generate($routeName, $routeParams, $queryParams, $fragmentIdentifier, $options);
+    }
+
+    /**
+     * Generate a fully qualified URI, relative to $path.
+     *
+     * @deprecated since 2.2.0; to be removed in 3.0.0. This method was originally
+     *     used internally to back the serverurl() Plates function; we now register
+     *     the ServerUrl instance directly, as it is callable.
+     */
+    public function generateServerUrl(string $path = null) : string
+    {
+        return $this->serverUrlHelper->generate($path);
+    }
 }
--- test/Extension/UrlExtensionTest.php
+++ test/Extension/UrlExtensionTest.php
@@ -54,4 +54,83 @@ class UrlExtensionTest extends TestCase
 
         $this->extension->register($engine->reveal());
     }
+
+    public function urlHelperParams()
+    {
+        return [
+            'null'             => [null, []],
+            'route-only'       => ['route', []],
+            'params-only'      => [null, ['param' => 'value']],
+            'route-and-params' => ['route', ['param' => 'value']],
+        ];
+    }
+
+    /**
+     * @dataProvider urlHelperParams
+     *
+     * @param null|string $route
+     * @param array $params
+     */
+    public function testGenerateUrlProxiesToUrlHelper($route, array $params)
+    {
+        $this->urlHelper->generate($route, $params, [], null, [])->willReturn('/success');
+        $this->assertEquals('/success', $this->extension->generateUrl($route, $params));
+    }
+
+    public function testUrlHelperAcceptsQueryParametersFragmentAndOptions()
+    {
+        $this->urlHelper->generate(
+            'resource',
+            ['id' => 'sha1'],
+            ['foo' => 'bar'],
+            'fragment',
+            ['reuse_result_params' => true]
+        )->willReturn('PATH');
+
+        $this->assertEquals(
+            'PATH',
+            $this->extension->generateUrl(
+                'resource',
+                ['id' => 'sha1'],
+                ['foo' => 'bar'],
+                'fragment',
+                ['reuse_result_params' => true]
+            )
+        );
+    }
+
+    public function serverUrlHelperParams()
+    {
+        return [
+            'null'          => [null],
+            'absolute-path' => ['/foo/bar'],
+            'relative-path' => ['foo/bar'],
+        ];
+    }
+
+    /**
+     * @dataProvider serverUrlHelperParams
+     *
+     * @param null|string $path
+     */
+    public function testGenerateServerUrlProxiesToServerUrlHelper($path)
+    {
+        $this->serverUrlHelper->generate($path)->willReturn('/success');
+        $this->assertEquals('/success', $this->extension->generateServerUrl($path));
+    }
+
+    public function testGetRouteResultReturnsRouteResultWhenPopulated()
+    {
+        $result = $this->prophesize(RouteResult::class);
+        $this->urlHelper->getRouteResult()->willReturn($result->reveal());
+
+        $this->assertInstanceOf(RouteResult::class, $this->extension->getRouteResult());
+    }
+
+    public function testGetRouteResultReturnsNullWhenRouteResultNotPopulatedInUrlHelper()
+    {
+        $this->urlHelper->getRouteResult()->willReturn(null);
+
+        $this->assertNull($this->extension->getRouteResult());
+    }
 }
